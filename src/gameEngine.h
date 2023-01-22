4e33c415f7d8867a1d91c74e3fdb6ae84e5d86c7
Endre <78541702+monkeysoop@users.noreply.github.com>
2023-01-22 19:20:49 +0100

    Update gameEngine.h

--- src/gameEngine.h
+++ src/gameEngine.h
@@ -11,9 +11,9 @@ void addToNeigbours(int* pcH, int h, int w, int WIDTH, int value);
 int countNeigbourMines(const int* pmi, int h, int w, int WIDTH, int HEIGHT);
 void countMines(const int* pmi, int* pcD, int WIDTH, int HEIGHT);
 int getStartingPos(const int* pmi, const int* pcD, int* ph, int* pw, int WIDTH, int HEIGHT);
-void exposeNeigbours(int* pma0, int h, int w, int WIDTH, int HEIGHT);
+void exposeNeigbours(int* emptySpaces, int* pma0, int h, int w, int WIDTH, int HEIGHT);
 int expandable(const int* pcH, const int* pma, int h, int w, int WIDTH, int HEIGHT);
-int expand(const int* pmi, int* pcH, int* pma, int h, int w, int WIDTH, int HEIGHT);
+int expand(int* emptySpaces, const int* pmi, int* pcH, int* pma, int h, int w, int WIDTH, int HEIGHT);
 void matrixCopy(const int* pfrom, int* pto, int WIDTH, int HEIGHT);
 void setBorder(int* pcD, int WIDTH, int HEIGHT);
 
