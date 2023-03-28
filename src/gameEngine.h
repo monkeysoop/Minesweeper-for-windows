b693cd8a70be7e7a84f3c8ed98451ed946d3fc72
Endre <78541702+monkeysoop@users.noreply.github.com>
2023-03-28 08:48:42 +0200

    Update gameEngine.h

--- src/gameEngine.h
+++ src/gameEngine.h
@@ -1,20 +1,19 @@
-#ifndef GAME_ENGINE_H
-#define GAME_ENGINE_H
+#ifndef NEW_GAME_ENGINE_H
+#define NEW_GAME_ENGINE_H
 
-void generateMines(int* pmi, int WIDTH, int HEIGHT, int db);
-int isMine(const int* pmi, int h, int w, int WIDTH);
-int numOfNeighbours(const int* pcH, int h, int w, int WIDTH);
+int isMine(int** pmi, int h, int w);
+int numOfNeighbours(int** pcH, int h, int w);
 int notBorder(int h, int w, int WIDTH, int HEIGHT);
-int notFlagg(const int* pma, int h, int w, int WIDTH);
-int notExposed(const int* pma, int h, int w, int WIDTH);
-void addToNeigbours(int* pcH, int h, int w, int WIDTH, int value);
-int countNeigbourMines(const int* pmi, int h, int w, int WIDTH, int HEIGHT);
-void countMines(const int* pmi, int* pcD, int WIDTH, int HEIGHT);
-int getStartingPos(const int* pmi, const int* pcD, int* ph, int* pw, int WIDTH, int HEIGHT);
-void exposeNeigbours(int* emptySpaces, int* pma0, int h, int w, int WIDTH, int HEIGHT);
-int expandable(const int* pcH, const int* pma, int h, int w, int WIDTH, int HEIGHT);
-int expand(int* emptySpaces, const int* pmi, int* pcH, int* pma, int h, int w, int WIDTH, int HEIGHT);
-void matrixCopy(const int* pfrom, int* pto, int WIDTH, int HEIGHT);
-void setBorder(int* pcD, int WIDTH, int HEIGHT);
+int notFlagg(int** pma, int h, int w);
+int notExposed(int** pma, int h, int w);
+void matrixCopy(int** pfrom, int** pto, int WIDTH, int HEIGHT);
+void generateMines(int** pmi, int WIDTH, int HEIGHT, int db);
+int countNeigbourMines(int** pmi, int h, int w, int WIDTH, int HEIGHT);
+void countMines(int** pmi, int** pcD, int WIDTH, int HEIGHT);
+int getStartingPos(int** pmi, int** pcD, int* ph, int* pw, int WIDTH, int HEIGHT);
+void addToNeigbours(int** pmi, int** pcH, int h, int w, int value);
+void exposeNeigbours(int* emptySpaces, int** pma, int h, int w, int WIDTH, int HEIGHT);
+int expandable(int** pcH, int** pma, int h, int w, int WIDTH, int HEIGHT);
+int expand(int* emptySpaces, int** pmi, int** pcH, int** pma, int h, int w, int WIDTH, int HEIGHT);
 
 #endif
